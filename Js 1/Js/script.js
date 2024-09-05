faa738a534d665b1b5daa85994a1dc438b09a699
Paremai <paremaifah@gmail.com>
2024-09-05 15:15:04 +0700

    Add files via upload

---
+++ Js 1/Js/script.js
@@ -0,0 +1,4 @@
+console.log('this is my script file'); //แสดงผลคอนโซล
+document.getElementById("text").innerHTML = "Paremaifah Rienthup"; //ข้อความที่ต้องการแสดง
+document.write("Hello document write");//แสดงข้อความยาวๆ
+window.alert("welcome to my page");//การเเจ้งเตือน
