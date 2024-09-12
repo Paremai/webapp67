543316cf771c340d6cbb13e376ab5c8fa9ae21e4
Paremai <paremaifah@gmail.com>
2024-09-12 15:18:09 +0700

    Add files via upload

---
+++ phpbasic/fanction26.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>writeMessess function</title>
+</head>
+<body>
+    <?php 
+    function writeMessage() //create function
+    { //start function
+        echo "this is first functin for write message";
+        echo "Have a nice program";
+    }//stop function
+
+    //call function
+    writeMessage();
+    ?>
+</body>
+</html>
